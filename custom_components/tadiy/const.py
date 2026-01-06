883a0104f01a9c8966f8f38a507cd8baca7746f8
Atsharim <atsharim@hotmail.de>
2026-01-06 11:28:07 +0100

    fix: Add missing const.py + logo support

---
+++ custom_components/tadiy/const.py
@@ -0,0 +1,14 @@
+"""Constants for TaDIY - Adaptive Climate Orchestrator."""
+
+DOMAIN = "tadiy"
+
+CONF_ROOMS = "rooms"
+CONF_ROOM_NAME = "room_name"
+CONF_TRV_ENTITIES = "trv_entities"
+CONF_MAIN_TEMP_SENSOR = "main_temp_sensor"
+CONF_WINDOW_SENSORS = "window_sensors"
+CONF_WEATHER_ENTITY = "weather_entity"
+CONF_OUTDOOR_SENSOR = "outdoor_sensor"
+CONF_WINDOW_OPEN_TIMEOUT = "window_open_timeout"
+CONF_WINDOW_CLOSE_TIMEOUT = "window_close_timeout"
+CONF_DONT_HEAT_BELOW_OUTDOOR = "dont_heat_below_outdoor"
